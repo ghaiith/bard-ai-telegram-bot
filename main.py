0650de5da214cabf526b5d866a398dd1a58a3732
Ghaith Ahmed <g.al3raqi@gmail.com>
2023-08-20 22:07:23 +0300

    fix some issues

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import google.generativeai as palm
 import telebot
 
-TOKEN = '6398171667:AAF_vYASuarTjlvgJQpfZjKuBv14ghTw-LI'
-palm.configure(api_key="AIzaSyBVvIdH4sMIWbAH3aIXKaMtBYJeUti--qQ")
+# Your Telegram bot token
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
 
+# Configure the generative AI API key
+palm.configure(api_key="YOUR_GENERATIVE_AI_API_KEY")
+
+# Create a Telegram bot instance
 bot = telebot.TeleBot(TOKEN)
 
-
+# Handle /start, /help, and /hello commands
 @bot.message_handler(commands=['start', 'help', 'hello'])
 def send_welcome(message):
     name = message.from_user.first_name
-    bot.reply_to(message, f"Hello {name}! Ask me About anything")
+    bot.reply_to(message, f"Hello {name}! Ask me about anything")
 
-
+# Handle all other messages
 @bot.message_handler(func=lambda message: True)
 def handle_search_product(message):
     name = message.from_user.first_name
     msg = message.text
+    
+    # Check for empty message
     if msg == "":
-        bot.reply_to(message, "Sorry, You mustn't write a empty message")
+        bot.reply_to(message, "Sorry, you mustn't write an empty message")
+    
     try:
-        bot.reply_to(message, f"Wait a Seconds {name} before send another message")
+        bot.reply_to(message, f"Please wait a moment, {name}, before sending another message")
+        
+        # Default parameters for the generative AI model
         defaults = {
             'model': 'models/chat-bison-001',
             'temperature': 0.25,
@@ -28,28 +37,34 @@ def handle_search_product(message):
             'top_k': 40,
             'top_p': 0.95,
         }
+        
         context = ""
         examples = [
             [
                 " ",
-
             ]
-
         ]
+        
         examples[0].append(str(msg))
         messages = []
         messages.append("NEXT REQUEST")
+        
+        # Generate response using the generative AI model
         response = palm.chat(
             **defaults,
             context=context,
             examples=examples,
             messages=messages
         )
+        
+        print(response.messages)
+        print(response.last)
         bot.reply_to(message, response.last)
+    
     except Exception as e:
+        bot.reply_to(message, str(e))
         bot.reply_to(message, "Sorry, an error occurred while processing your request.")
 
-
+# Start the bot's polling loop
 if __name__ == "__main__":
     bot.polling()
-
